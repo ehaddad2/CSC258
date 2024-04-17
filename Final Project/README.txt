4f3b9503eef039fcc854b7733672f174aeb608dc
stciko <93011238+stciko@users.noreply.github.com>
2024-04-17 18:22:50 -0400

    fw blocked

--- Final Project/README.txt
+++ Final Project/README.txt
@@ -9,7 +9,11 @@ https://en.wikipedia.org/wiki/Parallel_all-pairs_shortest_path_algorithm#:~:text
 
 Compile:
     g++ -std=c++11 -o ser fw_serial.cpp
+    g++ -std=c++11 -o par fw_parallel.cpp
+
 
 Run:
-    ./ser < test/mat5.txt
+    ./ser < test/geeks.txt b
+    ./par < test/mat5.txt b
+
 
--- Final Project/fw_parallel.cpp
+++ Final Project/fw_parallel.cpp
@@ -3,11 +3,17 @@
 #include <chrono>
 #include <algorithm>
 
+#ifdef _OPENMP
+#include "omp.h" // omp_get_num_threads
+#endif
+
 using namespace std;
 const int INF = numeric_limits<int>::max();
 
-int **mat;
+int **mat_in;
+int **mat_out;
 int n = 0;
+int b = 0;
 
 void printMatrix()
 {
@@ -15,10 +21,76 @@ void printMatrix()
 	{
 		for (int c = 0; c < n; c++)
 		{
-			cout << mat[r][c] << " ";
+			cout << mat_in[r][c] << " ";
 			;
 		}
 		cout << "\n";
+	}
+
+	for (int r = 0; r < n; r++)
+	{
+		for (int c = 0; c < n; c++)
+		{
+			cout << mat_out[r][c] << " ";
+			;
+		}
+		cout << "\n";
+	}
+}
+void floyd_warshall_in_place(int *C, const int *A, const int *B, const int b, const int n)
+{
+	for (int k = 0; k < b; k++)
+	{
+		int kth = k * n;
+		for (int i = 0; i < b; i++)
+		{
+			for (int j = 0; j < b; j++)
+			{
+				int sum = A[i * n + k] + B[kth + j];
+				if (C[i * n + j] > sum)
+				{
+					C[i * n + j] = sum;
+				}
+			}
+		}
+	}
+}
+
+void floyd_warshall_blocked(const int *input, int *output, const int n, const int b)
+{
+	std::memcpy(output, input, n * n * sizeof(int));
+
+	// for now, assume b divides n
+	const int blocks = n / b;
+
+	// note that [i][j] == [i * input_width * block_width + j * block_width]
+	for (int k = 0; k < blocks; k++)
+	{
+		floyd_warshall_in_place(&output[k * b * n + k * b], &output[k * b * n + k * b], &output[k * b * n + k * b], b, n);
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+		for (int j = 0; j < blocks; j++)
+		{
+			if (j == k)
+				continue;
+			floyd_warshall_in_place(&output[k * b * n + j * b], &output[k * b * n + k * b], &output[k * b * n + j * b], b, n);
+		}
+#ifdef _OPENMP
+#pragma omp parallel for
+#endif
+		for (int i = 0; i < blocks; i++)
+		{
+			if (i == k)
+				continue;
+			floyd_warshall_in_place(&output[i * b * n + k * b], &output[i * b * n + k * b], &output[k * b * n + k * b], b, n);
+			for (int j = 0; j < blocks; j++)
+			{
+				if (j == k)
+					continue;
+				floyd_warshall_in_place(&output[i * b * n + j * b], &output[i * b * n + k * b], &output[k * b * n + j * b], b, n);
+			}
+		}
 	}
 }
 
@@ -37,28 +109,8 @@ void floydWarshallParallel(int n, int p)
 	}
 }
 
-// Input: n - number of vertices
-// Output: Transformed a that contains the shortest path lengths
-void floydWarshallSerial(int n)
-{
-	// printMatrix();
 
-	for (int k = 0; k < n; k++)
-	{
-		for (int i = 0; i < n; i++)
-		{
-			for (int j = 0; j < n; j++)
-			{
-				if (mat[i][j] > (mat[i][k] + mat[k][j]) && (mat[k][j] != INF && mat[i][k] != INF))
-					mat[i][j] = mat[i][k] + mat[k][j];
-
-				// mat[i][j] = std::min(mat[i][j], mat[i][k] + mat[k][j]);
-			}
-		}
-	}
-	printMatrix();
-}
-
+// ./par < test/geeks.txt 2
 int main(int argc, char *argv[])
 {
 	using chrono::duration;
@@ -67,32 +119,34 @@ int main(int argc, char *argv[])
 	auto actualstart = high_resolution_clock::now();
 
 	int p = atoi(argv[1]);
+	int b = atoi(argv[2]);
 
 	cin >> n;
-	mat = new int *[n];
+	mat_in = new int *[n];
 
 	for (int i = 0; i < n; i++)
 	{
-		mat[i] = new int[n];
+		mat_in[i] = new int[n];
 
 		for (int j = 0; j < n; j++)
 		{
-			cin >> mat[i][j];
+			cin >> mat_in[i][j];
 
-			if (mat[i][j] == -1)
+			if (mat_in[i][j] == -1)
 			{
-				mat[i][j] = INF;
+				mat_in[i][j] = INF;
 			}
 		}
 	}
-
+	
 	auto fw_start = high_resolution_clock::now();
-	floydWarshallParallel(n, p);
+	// floydWarshallParallel(n, p);
+	floyd_warshall_blocked(mat_in, mat_out, n, b);
 
 	auto end = high_resolution_clock::now();
 	duration<double, milli> fw_time = end - fw_start;
 	duration<double, milli> actualtime = end - actualstart;
 
-	cout << "Serial time is: " << fw_time.count() << " milliseconds." << endl;
+	cout << "Parallel time is: " << fw_time.count() << " milliseconds." << endl;
 	cout << "Total time is: " << actualtime.count() << " milliseconds." << endl;
 }
